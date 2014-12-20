dbcee0b686dd12f74e497a1043677dc6752d7734
woonohyo <woonohyo@nhnnext.org>
2014-12-20 17:05:53 +0900

    Add Unsnap from MySnapsView

--- src/com/l3cache/snapshop/mysnaps/MySnapsAdapter.java
+++ src/com/l3cache/snapshop/mysnaps/MySnapsAdapter.java
@@ -2,44 +2,60 @@ package com.l3cache.snapshop.mysnaps;
 
 import java.util.ArrayList;
 
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+import retrofit.converter.GsonConverter;
 import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
 import com.l3cache.snapshop.AppController;
 import com.l3cache.snapshop.R;
+import com.l3cache.snapshop.SnapConstants;
+import com.l3cache.snapshop.SnapPreference;
 import com.l3cache.snapshop.newsfeed.Newsfeed;
+import com.l3cache.snapshop.retrofit.DefaultResponse;
+import com.l3cache.snapshop.retrofit.SnapShopService;
 import com.l3cache.snapshop.volley.ExtendedImageLoader;
 import com.l3cache.snapshop.volley.FeedImageView;
 
 public class MySnapsAdapter extends BaseAdapter {
 	private Activity activity;
 	private LayoutInflater inflater;
-	private ArrayList<Newsfeed> favoriteItems;
+	private ArrayList<Newsfeed> feedItems;
+	private MySnapsAdapter adapter;
+	private Button unsnapButton;
 	ExtendedImageLoader imageLoader = AppController.getInstance().getImageLoader();
 
 	public MySnapsAdapter(Activity activity, ArrayList<Newsfeed> feedItems) {
 		this.activity = activity;
-		this.favoriteItems = feedItems;
+		this.feedItems = feedItems;
+		adapter = this;
 	}
 
 	@Override
 	public int getCount() {
-		return favoriteItems.size();
+		return feedItems.size();
 	}
 
 	@Override
 	public Object getItem(int location) {
-		return favoriteItems.get(location);
+		return feedItems.get(location);
 	}
 
 	@Override
 	public long getItemId(int position) {
-		return position;
+		return feedItems.get(position).getPid();
 	}
 
 	@Override
@@ -53,7 +69,7 @@ public class MySnapsAdapter extends BaseAdapter {
 		if (imageLoader == null)
 			imageLoader = AppController.getInstance().getImageLoader();
 
-		Newsfeed item = favoriteItems.get(position);
+		Newsfeed item = feedItems.get(position);
 
 		FeedImageView feedImageView = (FeedImageView) convertView.findViewById(R.id.my_snap_image_view);
 		TextView writerTextView = (TextView) convertView.findViewById(R.id.my_snap_writer_text_view);
@@ -77,6 +93,45 @@ public class MySnapsAdapter extends BaseAdapter {
 		} else {
 			feedImageView.setVisibility(View.GONE);
 		}
+
+		unsnapButton = (Button) convertView.findViewById(R.id.my_snaps_unsnap_button);
+		unsnapButton.setTag(position);
+		unsnapButton.setOnClickListener(new OnClickListener() {
+			RestAdapter restAdapter = new RestAdapter.Builder().setEndpoint(SnapConstants.SERVER_URL)
+					.setConverter(new GsonConverter(new Gson())).build();
+			SnapShopService service = restAdapter.create(SnapShopService.class);
+			SnapPreference pref = new SnapPreference(activity);
+			int position;
+			int pid;
+
+			@Override
+			public void onClick(View v) {
+				position = (int) v.getTag();
+				pid = (int) getItemId(position);
+				service.unSnapPost(pref.getValue(SnapPreference.PREF_CURRENT_USER_ID, 0), pid,
+						new Callback<DefaultResponse>() {
+
+							@Override
+							public void success(DefaultResponse defResp, Response arg1) {
+								if (defResp.getStatus() == SnapConstants.SUCCESS) {
+									if (getCount() == 0)
+										return;
+									feedItems.remove(position);
+									adapter.notifyDataSetChanged();
+									Toast.makeText(activity, "Unsnap - " + pid, Toast.LENGTH_SHORT).show();
+								} else {
+									Toast.makeText(activity, "Unsnap Failed", Toast.LENGTH_SHORT).show();
+								}
+							}
+
+							@Override
+							public void failure(RetrofitError arg0) {
+								Toast.makeText(activity, "Network Error", Toast.LENGTH_SHORT).show();
+							}
+						});
+
+			}
+		});
 
 		return convertView;
 	}
